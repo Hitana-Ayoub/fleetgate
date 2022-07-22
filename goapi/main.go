aa74e153764b6f661a898a390cd478ff3db5bdc0
Ayoub Hitana <ayoubhitana@hotmail.com>
2022-07-22 13:20:06 +0100

    with gorilla mux

--- goapi/main.go
+++ goapi/main.go
@@ -1,25 +1,113 @@
 package main
 
 import (
-	"example/goapi/controllers"
-	"example/goapi/models"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
 )
 
+type Vehicle struct {
+	gorm.Model
+
+	Plate    string `json:"plate" gorm:"typevarchar(13); uniqueindex"`
+	Brand    string `json:"brand"`
+	Driver   string `json:"driver"`
+	Status   string `json:"status"`
+	Engine   string `json:"engine"`
+	KM       int    `json:"km"`
+	ImageURL string `json:"imageURL"`
+}
+
+var db *gorm.DB
+var err error
+
 func main() {
+	// Loading enviroment variables
+	dialect := "postgres"
+	host := "localhost"
+	dbPort := "5432"
+	user := "postgres"
+	dbname := "fleetgate"
+	dbpassword := "pgadmin"
 
-	router := gin.Default()
+	// Database connection string
+	dbURI := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable", host, dbPort, user, dbpassword, dbname)
 
-	routerVehicles := router.Group("/vehicles")
-	routerVehicles.GET("", controllers.GetVehicles)
-	routerVehicles.GET("/:id", controllers.GetVehicle)
-	routerVehicles.POST("", controllers.AddVehicle)
-	routerVehicles.PATCH("/:id", controllers.UpdateVehicle)
-	routerVehicles.DELETE("/:id", controllers.DeleteVehicle)
+	// Openning connection to database
+	db, err = gorm.Open(dialect, dbURI)
 
-	models.ConnectDatabase()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Connected to database successfully")
+	}
 
-	router.Run("localhost:9090")
+	// Close the databse connection when the main function closes
+	defer db.Close()
 
+	// Make migrations to the database if they haven't been made already
+	db.AutoMigrate(&Vehicle{})
+
+	/*----------- API routes ------------*/
+	router := mux.NewRouter()
+
+	router.HandleFunc("/vehicles", GetVehicles).Methods("GET")
+	router.HandleFunc("/vehicles/{id}", GetVehicle).Methods("GET")
+
+	router.HandleFunc("/vehicles", CreateVehicle).Methods("POST")
+
+	router.HandleFunc("/vehicles/{id}", DeleteVehicle).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+/*-------- API Controllers --------*/
+
+/*----- People ------*/
+func GetVehicle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var vehicle Vehicle
+
+	db.First(&vehicle, params["id"])
+
+	json.NewEncoder(w).Encode(&vehicle)
+}
+
+func GetVehicles(w http.ResponseWriter, r *http.Request) {
+	var vehicle []Vehicle
+
+	db.Find(&vehicle)
+
+	json.NewEncoder(w).Encode(&vehicle)
+}
+
+func CreateVehicle(w http.ResponseWriter, r *http.Request) {
+	var vehicle Vehicle
+	json.NewDecoder(r.Body).Decode(&vehicle)
+
+	createdVehicle := db.Create(&vehicle)
+	err = createdVehicle.Error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	json.NewEncoder(w).Encode(&createdVehicle)
+}
+
+func DeleteVehicle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	var vehicle Vehicle
+
+	db.First(&vehicle, params["id"])
+	db.Delete(&vehicle)
+
+	json.NewEncoder(w).Encode(&vehicle)
 }
